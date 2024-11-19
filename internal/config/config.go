f7b99fb1fee1d8c6946c0d6fa6aff83531aaadd8
SeanH <85364282+SeanHuebl@users.noreply.github.com>
2024-11-19 12:11:06 +0000

    started working on handles, and the command that will add other commands to the cli

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,47 +14,78 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
+func (c *Config) SetUser(username string) error {
+	config := Read()
+	config.CurrentUserName = username
+	jdata, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error marshalling to JSON: %v", err)
+	}
+	filepath := getConfigFilePath()
+	file, err := os.Open(filepath)
+	if err != nil {
+		return fmt.Errorf("error opening file at location %v", filepath)
+	}
+	defer file.Close()
+	err = os.WriteFile(filepath, jdata, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing: %v", err)
+	}
+	return nil
+}
+
+type Commands struct {
+	commands map[string]func(*State, Command) error
+}
+
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	c.commands[name] = f
+}
+
+type Command struct {
+	name      string
+	arguments []string
+}
+
+type State struct {
+	ConfigPtr *Config
+}
+
+func handlerLogin(s *State, cmd Command) error {
+	if len(cmd.arguments) == 0 {
+		return fmt.Errorf("login expects one argument: username")
+	}
+
+	err := s.ConfigPtr.SetUser(cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("%s", err)
+	}
+	return nil
+}
+
+
 func getConfigFilePath() string {
 	homeDir, _ := os.UserHomeDir()
 	return homeDir + configFileName
 }
 
-func (c *Config) SetUser(username string) {
-	config := Read()
-	config.CurrentUserName = username
-	// need to marshal data to []byte then write to the json file
-	jdata, err := json.MarshalIndent(config, "", "  ")
-	if err != nil {
-		fmt.Printf("Error marshalling to JSON: %v\n", err)
-	}
-	filepath := getConfigFilePath()
-	file, err := os.Open(filepath)
-	if err != nil {
-		fmt.Printf("Error opening file at location %v\n", filepath)
-	}
-	defer file.Close()
-	err = os.WriteFile(filepath, jdata, 0644)
-	if err != nil {
-		fmt.Printf("Error writing: %v\n", err)
-	}
-}
 
 func Read() Config {
 
 	filepath := getConfigFilePath()
 	file, err := os.Open(filepath)
 	if err != nil {
-		fmt.Printf("Error opening file at location: %v\n", filepath)
+		fmt.Printf("error opening file at location: %v\n", filepath)
 	}
 	defer file.Close()
 	var config Config
 	data, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("Error reading file data")
+		fmt.Println("error reading file data")
 	}
 
 	if err = json.Unmarshal(data, &config); err != nil {
-		fmt.Println("Unable to parse data to struct")
+		fmt.Println("unable to parse data to struct")
 	}
 	return config
 }
